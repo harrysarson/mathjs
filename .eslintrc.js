95b007bd627d5118193676b27eb1b142368a4a82
Harry Sarson <harry.sarson@hotmail.co.uk>
2018-04-27 22:23:34 +0100

    keeping messing

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,4 +6,28 @@ module.exports = {
     browser: true,
     es6: true,
   },
+  rules: {
+    indent: ['error', 2, {
+      SwitchCase: 1,
+      VariableDeclarator: 1,
+      outerIIFEBody: 1,
+      // MemberExpression: null,
+      FunctionDeclaration: {
+        parameters: 1,
+        body: 1
+      },
+      FunctionExpression: {
+        parameters: 1,
+        body: 1
+      },
+      CallExpression: {
+        arguments: 1
+      },
+      ArrayExpression: 1,
+      ObjectExpression: 1,
+      ImportDeclaration: 1,
+      flatTernaryExpressions: false,
+      ignoreComments: false
+    }],
+  }
 };
