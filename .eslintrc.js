f0ecafbea2b90796d5c8e28ff5d1e23c4d4d14a1
Harry Sarson <harry.sarson@hotmail.co.uk>
2018-04-28 11:49:03 +0100

    lint for eol-last

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,27 +7,6 @@ module.exports = {
     es6: true,
   },
   rules: {
-    indent: ['error', 2, {
-      SwitchCase: 1,
-      VariableDeclarator: 1,
-      outerIIFEBody: 1,
-      // MemberExpression: null,
-      FunctionDeclaration: {
-        parameters: 1,
-        body: 1
-      },
-      FunctionExpression: {
-        parameters: 1,
-        body: 1
-      },
-      CallExpression: {
-        arguments: 1
-      },
-      ArrayExpression: 1,
-      ObjectExpression: 1,
-      ImportDeclaration: 1,
-      flatTernaryExpressions: false,
-      ignoreComments: false
-    }],
+    'eol-last': ['error', 'always'],
   }
 };
